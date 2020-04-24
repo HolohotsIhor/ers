2cb6b35d100d4968f02de0ef8ae680043904ca27
Holohots <igor.gologots@hyuna.bb>
2020-04-24 19:58:45 +0300

    Finished 1st day

---
+++ app/js/common.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+
+    $("#employeer").click(function() {
+        ckeckBoxStatus = $("#employeer").is(':checked');
+
+        if (ckeckBoxStatus == true) {
+            $('.lookiner').css("display", "none");
+            $('.employers').css("display", "block");
+            $('.employeer-form-box').css("display", "block");
+            $('.employee-form-box').css("display", "none");
+            $('#employee').removeAttr('checked');
+        }
+
+        if (ckeckBoxStatus == false) {
+
+        }
+    });
+
+    $("#employee").click(function() {
+        ckeckBoxStatus = $("#employee").is(':checked');
+
+        if (ckeckBoxStatus == true) {
+            $('.lookiner').css("display", "block");
+            $('.employers').css("display", "none");
+            $('.employee-form-box').css("display", "block");
+            $('.employeer-form-box').css("display", "none");
+            $('#employeer').removeAttr('checked');
+        }
+
+        if (ckeckBoxStatus == false) {
+
+        }
+    });
+
+
+    /* Scrool animation */
+    $(window).scroll(function() {
+
+        $('.mov-left').each(function() {
+            var imagePos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+            if (imagePos < topOfWindow + 600) {
+                $(this).addClass('fadeInLeft animated');
+                $(this).css('opacity', '1');
+            }
+        });
+
+
+    });
+});
